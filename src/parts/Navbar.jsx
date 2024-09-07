1024882ee472c48a1939ca8292b3ac80af49492f
Mohmammed Nizar <mohmammednizar@Mohmammeds-MBP.lan>
2024-09-07 16:02:21 -0400

    made nav bar

---
+++ src/parts/Navbar.jsx
@@ -0,0 +1,26 @@
+export default function Navbar() {
+  return (
+    <div className="w-full px-4 py-2 flex h-100 items-center justify-between bg-gray-100">
+      
+      <div className="w-60 ml-7">
+        <a href="/" className="ml-auto hover:opacity-60  ">
+          <img src="/public/nizlogo.svg" alt="Logo" className="h-12 w-12 rounded-md" />
+        </a>
+      </div>
+      
+      <div className="flex gap-7 ml-auto mr-7 ">
+        <a href="/about/" className="hover:opacity-60 font-bold text-lg">
+          About
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1CG_1_6czo1jvsKKgYUlrKkAtKXenQZ8U/view"
+          className="hover:opacity-60 font-bold text-lg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
+      </div>
+    </div>
+  );
+}
